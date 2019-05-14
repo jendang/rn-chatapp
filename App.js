709062bb67c79a76dee10749dc4acd69ff6b0735
jennydang <jenny.dang117@gmail.com>
2019-05-14 16:25:49 +0200

    form for signup

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  //StyleSheet, 
+  //Text, 
+  View 
+} from 'react-native';
+import styles from './App.styles'
+import SignUp from './screens/SignUp'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <SignUp />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); */
